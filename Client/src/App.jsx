e2da497bbf3306a47ced534a2429e70f677d82f9
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-11-05 23:27:57 -0500

    Working on AuthForm component more

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 // We will be combining the login and register forms into one which will be defined by our routes
 // If we are on "/login" it'll be the login form, and if we're on "/register" we will be on the register form (but again, it's all being done by the same component - we could always make a separate component, but we're a badass fullstack dev)
@@ -9,7 +9,7 @@ import { Route, Routes } from 'react-router-dom'
 // CODE BREAK DOWN *****
 // We have two useStates for our username/password
 // We will store the values from our input fields into them
-// Inside the input, we need to set the value to the state name to connect them
+// Inside the input, we need to set the value to the state name which connects them
 // Then we have the "onChange" which is an eventlistener (ev) that sets the username to whatever is in the text field
 // This is because we say that the value in the text field is = to username - and in the onchange we say target the value, and set that value as the username (setUsername(ev.target.value)) 
 
@@ -18,25 +18,36 @@ import { Route, Routes } from 'react-router-dom'
 // So we have two fields (inputs) inside a form, and when you click the submit button, the "onSubmit" will be called
 // We put a "event.preventDefault();" in the onSubmit because the default behavior for form submissions is refreshing (for some dumb reason) and this method prevents the default behavior hence "preventDefault" lol
 
-const AuthForm = () => {
+const AuthForm = ({ onAuthSubmit }) => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const location = useLocation();
+
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
+
+    // console.log('Username:', username);
+    // console.log('Password:', password);
+
+    onAuthSubmit(username, password);
+
   };
 
   return (
     <form onSubmit={onSubmit}>
       <input placeholder="Username" value = { username } onChange = {ev => setUsername(ev.target.value)} />
       <input placeholder="Password" value = { password } onChange = {ev => setPassword(ev.target.value)} />
-      <button>Submit</button>
+      <button>{location.pathname == "/login" ? "Login" : "Register"}</button>
     </form>
   );
 };
+
+// The location hook allows us to change code based on our current location, such as our pathname in the URL (ex: /login or /register)
+// So for button we used a ternary operator (if/else) that says IF the locations pathname is equal to "/login" then display "Login" on the button, but if the pathname isn't "/login", aka "/register" (or anything else for that matter) then display "Register" on the button instead
+
+
 
 // APP CODE/ROUTES BREAKDOWN ******
 // We have the App that's invoked which auto initializes all the code inside of it once the page is ran
@@ -47,11 +58,18 @@ const AuthForm = () => {
 
 function App() {
 
+const login = () => {
+  console.log('Login button clicked');
+};
+const register = () => {
+  console.log('Register button clicked');
+};
+
   return (
     <>
      <Routes>
-      <Route path="/login" element={<AuthForm />} />
-      <Route path="/register" element={<AuthForm />} />
+      <Route path="/login" element={<AuthForm onAuthSubmit={ login } />} />
+      <Route path="/register" element={<AuthForm onAuthSubmit={ register } />} />
      </Routes>
     </>
   );
